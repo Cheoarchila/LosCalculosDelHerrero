c53aed6e353e8c7ed0448a2e7fbd939839b57567
Cheoarchila <86863465+Cheoarchila@users.noreply.github.com>
2026-07-28 15:21:14 -0500

    Refactor mostrarModulo to abrirAcanalado function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function mostrarModulo(nombre){
+function abrirAcanalado(){
 
-    alert("Módulo seleccionado:\n\n" + nombre + "\n\nLo construiremos en las siguientes versiones.");
+    document.getElementById("inicio").style.display="none";
+
+    document.getElementById("acanalado").style.display="block";
+
+}
+
+function volverInicio(){
+
+    document.getElementById("acanalado").style.display="none";
+
+    document.getElementById("inicio").style.display="block";
 
 }
